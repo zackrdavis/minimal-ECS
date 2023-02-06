b52599feed4d93af636418c5d4d88a3717998340
Zack Davis <zackrdavis@gmail.com>
2023-02-06 15:41:39 -0800

    remove counter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import "./style.css";
 import typescriptLogo from "./typescript.svg";
-import { setupCounter } from "./counter";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -12,13 +11,8 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     </a>
     <h1>Vite + TypeScript
     </h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
 `;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
