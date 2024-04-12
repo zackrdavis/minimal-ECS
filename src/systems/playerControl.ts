4bf17b1e7b96a31dee904b7cec70bd1f41e6fc97
Zack Davis <zackrdavis@gmail.com>
2024-04-12 16:48:16 -0700

    add wasd controls and update index style/text

--- src/systems/playerControl.ts
+++ src/systems/playerControl.ts
@@ -1,3 +1,4 @@
+import { s } from "vitest/dist/env-afee91f0";
 import { Ent, forEntsWith } from "./shared";
 
 const clamp = (num: number, min: number, max: number) => {
@@ -10,10 +11,16 @@ export class PlayerControlSystem {
     ArrowDown: false,
     ArrowLeft: false,
     ArrowRight: false,
+    w: false,
+    a: false,
+    s: false,
+    d: false,
   };
 
   constructor() {
     window.addEventListener("keydown", (e) => {
+      console.log(e.key);
+
       const pressedKey = e.key as keyof typeof this.keys;
 
       if (pressedKey in this.keys) {
@@ -32,20 +39,22 @@ export class PlayerControlSystem {
 
   update(entities: Ent[]) {
     forEntsWith(["velocity", "playerControl"], entities, (entity) => {
-      const { ArrowUp, ArrowDown, ArrowLeft, ArrowRight } = this.keys;
+      const { ArrowUp, ArrowDown, ArrowLeft, ArrowRight, w, a, s, d } =
+        this.keys;
+
+      const right = ArrowRight || d;
+      const left = ArrowLeft || a;
+      const up = ArrowUp || w;
+      const down = ArrowDown || s;
+
       const { acceleration, maxSpeed } = entity.playerControl;
 
-      if (
-        (ArrowRight || ArrowLeft || ArrowDown || ArrowUp) &&
-        !entity.collisionEvent
-      ) {
+      if ((right || left || down || up) && !entity.collisionEvent) {
         // accelerate up to maxSpeed
         const { x, y } = entity.velocity;
 
-        const newXVel =
-          x + (ArrowRight ? acceleration : ArrowLeft ? -acceleration : 0);
-        const newYVel =
-          y + (ArrowDown ? acceleration : ArrowUp ? -acceleration : 0);
+        const newXVel = x + (right ? acceleration : left ? -acceleration : 0);
+        const newYVel = y + (down ? acceleration : up ? -acceleration : 0);
 
         entity.velocity = {
           x: clamp(newXVel, -maxSpeed, maxSpeed),
