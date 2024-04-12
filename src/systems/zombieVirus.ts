faba86b9a1035e14daa237f7e38680b1994c4dbc
Zack Davis <zackrdavis@gmail.com>
2024-04-12 15:25:27 -0700

    simplify zombieVirus system

--- src/systems/zombieVirus.ts
+++ src/systems/zombieVirus.ts
@@ -8,13 +8,8 @@ export class ZombieVirus {
       (entity1) => {
         const entity2 = entity1.collisionEvent.entity;
 
-        const { x: x1, y: y1 } = entity1.velocity;
-        const { x: x2, y: y2 } = entity2.velocity || { x: 0, y: 0 };
-        const speed1 = Math.sqrt(x1 ** 2 + y1 ** 2);
-        const speed2 = Math.sqrt(x2 ** 2 + y2 ** 2);
-
-        // if zombie (entity1) has a higher speed than entity2, transmit zombieVirus
-        if (speed1 > speed2 && entity2?.style?.color) {
+        // if both entities have velocity and color, infect the second entity
+        if (entity2?.velocity && entity2?.style?.color) {
           entity2.zombieVirus = true;
           entity2.style.color = "green";
         }
