da5988f7d151b466f2255b5faed0442e154f5889
Zack Davis <zackrdavis@gmail.com>
2024-05-20 11:44:48 -0700

    test for collisionSystem

---
+++ src/systems/collisionSystem.test.ts
@@ -0,0 +1,55 @@
+import { expect, test } from "vitest";
+import { collisionSystem } from "./collisionSystem";
+import { Entity } from "../types";
+
+test("CollisionSystem registers collision and saves overlaps", () => {
+  const ent1: Entity = {
+    id: "1",
+    position: { x: 0, y: 0 },
+    collisionBox: { width: 10, height: 10, collisions: [] },
+  };
+
+  const ent2: Entity = {
+    id: "2",
+    position: { x: 5, y: 0 },
+    collisionBox: { width: 10, height: 10, collisions: [] },
+  };
+
+  const exp1: Entity = {
+    id: "1",
+    position: { x: 0, y: 0 },
+    collisionBox: {
+      width: 10,
+      height: 10,
+      collisions: [
+        {
+          otherEntId: "2",
+          xOverlap: 5,
+          yOverlap: 10,
+        },
+      ],
+    },
+  };
+
+  const exp2: Entity = {
+    id: "2",
+    position: { x: 5, y: 0 },
+    collisionBox: {
+      width: 10,
+      height: 10,
+      collisions: [
+        {
+          otherEntId: "1",
+          xOverlap: -5,
+          yOverlap: 10,
+        },
+      ],
+    },
+  };
+
+  collisionSystem([ent1, ent2]);
+
+  expect(ent1).toEqual(exp1);
+
+  expect(ent2).toEqual(exp2);
+});
